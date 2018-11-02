2798700d423a3af9416ba777e782aaacc2426395
Jon Berliner <jsberliner@gmail.com>
2018-11-02 13:52:01 -0400

    varianted and commented

--- datasets.py
+++ datasets.py
@@ -5,31 +5,50 @@ import json
 import pandas as pd
 import numpy as np
 from typing import List,\
-                   Dict
+                   Dict,\
+                   Callable
 
-class ProductDataset(Dataset):
-    ATTRIBUTES_QUERY_TEMPLATE =\
+class VariantDataset(Dataset):
+    INFO_QUERY_TEMPLATE =\
         """
         SELECT
           *
         FROM
           porsche_data.variants
         WHERE
-          id IN {:s}
-        LIMIT
-          10000
+          variant_id IN {:s}
         """
 
     def __init__(self,
-                 product_uids: List,
+                 variant_uids: List,
+                 transforms: Callable,
                  path: str,
-                 download: bool,
-                 return_image: bool=False) -> None:
+                 download: bool) -> None:
         """
-        dataset for ss products
+        dataset for ss variants
+
+        Args:
+            variant_uids (List):
+                the list of variant_ids for product variants to grab from
+                porsche_data.variants
+
+            transforms (Callable):
+                function that will take in the query results and process into
+                whatever form (eg a vector representation) will be served by
+                the dataset when __getitem__ called
+
+            # NOTE: in future, would make to be able to not have to save
+            #       locally, and stream directly from DB to the dataset.
+            #       doing must save local now to get started somewhere
+            path (str):
+                where pulled data is going to be stored locally
+
+            download (bool):
+                should we download and save the query results, or have we
+                already downloaded?
         """
-        # NOTE: the unique id "id" in porsche_data.variants, not "product_id"
-        self.product_uids = product_uids
+        super().__init__()
+        self.variant_uids = variant_uids
         self.path = path
         self.return_image = return_image
 
@@ -40,25 +59,27 @@ class ProductDataset(Dataset):
             bq = BigQueryClient()
 
             print('querying bigquery for data...')
-            attributes_query = self.ATTRIBUTES_QUERY_TEMPLATE\
-                    .format(''.join(['(', str(self.product_uids)[1:-1], ')']))
-            product_attributes = bq.run_query(attributes_query)
+            info_query = self.INFO_QUERY_TEMPLATE\
+                    .format(''.join(['(', str(self.variant_uids)[1:-1], ')']))
+            variant_info = bq.run_query(info_query)
             print('saving to {:s}...'.format(self.path))
-            product_attributes.to_json(self.path)
+            variant_info.to_json(self.path)
         else:
             assert os.path.exists(self.path),\
                 "dataset {:s} does not exist.  change path or set download=True"
         print('loading dataset from {:s}...'.format(self.path))
-        product_attributes = pd.read_json(self.path)
-        self.product_attributes = product_attributes.set_index('variant_id')
+        variant_info = pd.read_json(self.path)
+        self.variant_info = variant_info.set_index('variant_id')
+
+        self.data = self.transforms(self.variant_info)
 
 
-    def __get_item__(self, index: int) -> Dict:
-        uid = self.product_uids[index]
-        return self.product_attributes.loc[uid]
+    def __getitem__(self, index: int) -> Dict:
+        uid = self.variant_uids[index]
+        return self.variant_info.loc[uid]
 
     def __len__(self):
-        return len(self.product_uids)
+        return len(self.variant_uids)
 
 
 if __name__ == '__main__':
@@ -77,23 +98,26 @@ if __name__ == '__main__':
     bq = BigQueryClient()
     IDS = bq.run_query(IDS_QUERY).values.ravel().tolist()
 
-    dataset = ProductDataset(product_uids=IDS,
-                             path='test_data/test_path.json',
-                             download=True)
+    # dataset = VariantDataset(variant_uids=IDS,
+    #                          transforms=lambda x: x,
+    #                          path='test_data/test_path.json',
+    #                          download=True)
 
-    dataset = ProductDataset(product_uids=IDS,
+    dataset = VariantDataset(variant_uids=IDS,
+                             transforms=lambda x: x,
                              path='test_data/test_path.json',
                              download=False)
+    data_loader = DataLoader(dataset)
 
 
 
-    # TRYING TO UNDERSTAND ATTRIBUTES OF THE DATA
+    # TRYING TO UNDERSTAND INFO OF THE DATA
 
     # maybe constant by affiliate?  trying with linkshare
-    df = dataset.product_attributes
+    df = dataset.variant_info
     linkshare_uids = df[df.affiliate_id == "linkshare"].affiliate_id
     linkshare_inds = np.where([uid in linkshare_uids for uid in IDS])[0]
-    lsp = [dataset.__get_item__(ii) for ii in linkshare_inds]
+    lsp = [dataset.__getitem__(ii) for ii in linkshare_inds]
 
     def parse_fn(lsp):
         json.loads(lsp.raw)['attributeClass']['Product_Type']
