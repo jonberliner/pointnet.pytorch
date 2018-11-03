1eb13a4b06b480e5883e418bb3dd5922232287fc
Jon Berliner <jsberliner@gmail.com>
2018-11-03 14:10:28 -0400

    removing things not for extending to semantic nd locations

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# extending pointnet to nd
+original readme below
+
 # PointNet.pytorch
 This repo is implementation for PointNet(https://arxiv.org/abs/1612.00593) in pytorch. The model is in `pointnet.py`.
 
@@ -14,7 +17,7 @@ python show_seg.py --model seg/seg_model_20.pth  # show segmentation results
 ```
 
 # Performance
-Without heavy tuning, PointNet can achieve 80-90% performance in classification and segmentaion on this [dataset](http://web.stanford.edu/~ericyi/project_page/part_annotation/index.html). 
+Without heavy tuning, PointNet can achieve 80-90% performance in classification and segmentaion on this [dataset](http://web.stanford.edu/~ericyi/project_page/part_annotation/index.html).
 
 Sample segmentation result:
 ![seg](https://raw.githubusercontent.com/fxia22/pointnet.pytorch/master/misc/show3d.png?token=AE638Oy51TL2HDCaeCF273X_-Bsy6-E2ks5Y_BUzwA%3D%3D)
--- jb_train_classification.py
+++ jb_train_classification.py
@@ -7,7 +7,11 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
+from torch.utils.data import DataLoader
+
 from jb_pointnet import PointNetCls
+from lsk.data.datasets import VariantDataset
+
 
 parser = argparse.ArgumentParser()
 # TODO: should be info in the dataset
@@ -30,64 +34,88 @@ try:
 except OSError:
     pass
 
-if opt.random_seed < 1:
+if opt.random_seed < 0:
     opt.random_seed = random.randint(1e8, 1e9-1)
 
 print (opt)
 
-# blue = lambda x:'\033[94m' + x + '\033[0m'
-
 torch.manual_seed(opt.random_seed)
 
-dataset = ...
-dataloader = torch.utils.data.DataLoader(dataset,
-                                         batch_size=opt.batch_size,
-                                         shuffle=True,
-                                         num_workers=int(opt.workers))
-
-test_dataset = ...
-test_dataloader = torch.utils.data.DataLoader(test_dataset,
-                                              batch_size=opt.batch_size,
-                                              shuffle=False,
-                                              num_workers=int(opt.workers))
+# BUILD DATASET
+TRAINING_UIDS = ...
+TESTING_UIDS = ...
 
 
+def lookup_embedding(variant_info):
+    attributes = extract_attributes(variant_info)
+
+
+dataset = VariantDataset(variant_uids=TRAINING_UIDS,
+                         transforms=transform,
+                         path=args.data_path,
+                         download=True)
+data_loader = DataLoader(dataset,
+                         batch_size=opt.batch_size,
+                         shuffle=True,
+                         num_workers=int(opt.workers))
+
+test_dataset = VariantDataset(variant_uids=TRAINING_UIDS,
+                              transforms=transform,
+                              path=args.test_data_path,
+                              download=True)
+test_data_loader = DataLoader(dataset,
+                              batch_size=opt.batch_size,
+                              shuffle=True,
+                              num_workers=int(opt.workers))
+
+
+# GPU training?
 CUDA = torch.cuda.is_available()
 device = 'cuda' if CUDA else 'cpu'
 
+# BUILD MODEL
 classifier = PointNetCls(dim_input=dim_input,
-                         num_classes=num_classes)
-optimizer = optim.SGD(classifier.parameters(), lr=0.01, momentum=0.9)
+                         num_classes=num_classes)\
+                        .to(device)
 
+# (maybe) load checkpoint
 if opt.model_checkpoint != '':
     classifier.load_state_dict(torch.load(opt.model_checkpoint))
 
-classifier.to(device)
+# instantiate optimizer
+optimizer = optim.SGD(classifier.parameters(), lr=0.01, momentum=0.9)
 
+# TRAINING LOOP
 num_batch = len(dataset) // opt.batchSize
-
 for epoch in range(opt.nepoch):
-    for i, data in enumerate(dataloader, 0):
+    for step_in_epoch, data in enumerate(data_loader, 0):
+
+        # get data
         points, target = data
         points = points.transpose(2, 1)
         points, target = points.to(device), target.to(device)
 
+        # forward pass
         optimizer.zero_grad()
         classifier = classifier.train()
         pred, _ = classifier(points)
+        # calculate loss
         loss = F.nll_loss(pred, target)
+        # backward pass
         loss.backward()
+        # update
         optimizer.step()
+        # print step performance
         pred_choice = pred.data.max(1)[1]
         correct = pred_choice.eq(target.data).cpu().sum()
         print('[%d: %d/%d] train loss: %f accuracy: %f' %
               (epoch,
-               i,
+               step_in_epoch,
                num_batch,
                loss.item(),correct.item() / float(opt.batch_size)))
 
-        if i % 10 == 0:
-            j, data = next(enumerate(test_dataloader, 0))
+        if step_in_epoch % 10 == 0:
+            step_in_test, data = next(enumerate(test_data_loader, 0))
             points, target = data
             points = points.transpose(2, 1)
             points, target = point.to(device), target.to(device)
@@ -98,7 +126,7 @@ for epoch in range(opt.nepoch):
             correct = pred_choice.eq(target.data).cpu().sum()
             print('[%d: %d/%d] %s loss: %f accuracy: %f' %
                   (epoch,
-                   i,
+                   step_in_test,
                    num_batch,
                    blue('test'),
                    loss.item(),
@@ -108,3 +136,4 @@ for epoch in range(opt.nepoch):
                '%s/cls_model_%d.pth' % (opt.output_dir, epoch))
     torch.save(embeddings.state_dict(),
                '%s/cls_embeddings_%d.pth' % (opt.output_dir, epoch))
+
